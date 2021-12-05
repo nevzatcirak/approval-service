7e0cef09382a3ad8238fe3f1e181857b8d4045ce
NvztCRK <nevzatcirak17@hotmail.com>
2021-12-05 16:58:16 +0300

    initial commit

---
+++ src/main/java/me/nevzatcirak/service/approval/ApprovalApplication.java
@@ -0,0 +1,13 @@
+package me.nevzatcirak.service.approval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprovalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApprovalApplication.class, args);
+	}
+
+}
