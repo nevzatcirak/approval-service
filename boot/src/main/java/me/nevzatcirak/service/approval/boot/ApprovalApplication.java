3cea7b715f82503b03af8d6c1a4b6a7e173a1e36
Nevzat ÇIRAK <ncirak@havelsan.com.tr>
2021-12-06 13:56:08 +0300

    Project module rearrangement

--- boot/src/main/java/me/nevzatcirak/service/approval/boot/ApprovalApplication.java
+++ boot/src/main/java/me/nevzatcirak/service/approval/boot/ApprovalApplication.java
@@ -1,4 +1,4 @@
-package me.nevzatcirak.service.approval;
+package me.nevzatcirak.service.approval.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
